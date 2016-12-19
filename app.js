9fd4974960884020c47fbc3fcb0006cc5cf70c13
StreakInTheSky <streakinthesky@gmail.com>
2016-12-18 18:55:45 -0800

    Add and edit functions to use check and delete buttons, needs fix to rendering after delete

--- app.js
+++ app.js
@@ -6,31 +6,70 @@ function main() {
 	// store all jQuery elements needed
 	function storeItems(state, cartItem) {
 		cartItem.each(function(index) {
-			state.items[index] = $(this).text();
+			var itemObject = {};
+			itemObject['name'] = $(this).text();
+			if ($(this).hasClass('shopping-item__checked')) {
+				itemObject['checked'] = true;
+			} else {
+				itemObject['checked'] = false;
+			}
+			state.items.push(itemObject);
 		})
 	}
-	function checkIfChecked(state, cartItem){
 
-	}
 	// input, form, ul (that contains the list items), 
 	function addItem(state, item) {
-		state.items.push(item);
+		var newObject = {
+			name: item,
+			checked: false
+		}
+		state.items.push(newObject);
+	}
+
+	function deleteItem(state, item) {
+		$(item).remove();
+	}
+
+	function attachEventHandlers() {
+		$('.shopping-item-delete').click(function(event) {
+			var deleteIndex = $(event.target).closest('li').index();
+			state.items.splice(deleteIndex, 1);
+			renderList(state, $('.shopping-list'));
+		})
+	}
+
+	function initFormHandler() {
+		$('#js-shopping-list-form').submit(function(event) {
+		event.preventDefault();
+		addItem(state, $('input[name=shopping-list-entry]').val());
+		renderList(state, $('.shopping-list'));
+		})
 	}
 
 	var renderList = function(state, element){
 		var itemsHTML = state.items.map(function(item) {
-			return '<li>' + item + '</li>';
+			return ('<li>' +
+        '<span class="shopping-item' + (item.checked ? ' shopping-item__checked' : '') +'">' + item.name + '</span>' +
+        '<div class="shopping-item-controls">' +
+          '<button class="shopping-item-toggle">' +
+            '<span class="button-label">check</span>' +
+          '</button>' +
+          '<button class="shopping-item-delete">' +
+            '<span class="button-label">delete</span>' +
+          '</button>' +
+        '</div>' +
+				'</li>');
 		});
+
 		element.html(itemsHTML);
+		attachEventHandlers();
 	};
 
-	$('#js-shopping-list-form').submit(function(event) {
-		event.preventDefault();
-		storeItems(state, $('.shopping-item'));
-		addItem(state, $('input[name=shopping-list-entry]').val());
-		renderList(state, $('.shopping-list'));
-	})
-
+	//run on startup
+	storeItems(state, $('.shopping-item'));
+	renderList(state, $('.shopping-list'));
+	initFormHandler();
+	attachEventHandlers();
 }
 
 $(document).ready(main);
